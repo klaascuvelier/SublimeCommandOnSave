7e867e9cab472207e66c5f521bf4b68922d8c680
Klaas Cuvelier <klaas@showpad.com>
2013-06-10 19:51:07 +0200

    settings now is a map, and support for multiple commands per folder

--- CommandOnSave.py
+++ CommandOnSave.py
@@ -6,14 +6,16 @@ import subprocess
 class CommandOnSave(sublime_plugin.EventListener):
     def on_post_save(self, view):
 
-        settings = sublime.load_settings('CommandOnSave.sublime-settings')
-        commands = settings.get('commands')
+        settings = sublime.load_settings('CommandOnSave.sublime-settings').get('commands')
         file = view.file_name()
 
-        if not commands == None:
-            for entry in commands:
-                path, cmd = entry.split('::', 1)
-                if file.startswith(path) and len(cmd) > 0:
-                    p = subprocess.Popen([cmd], shell=True, stdout=subprocess.PIPE)
-                    out, err = p.communicate()
-                    print ("Command on Save:\n" + out.decode('utf-8'))
+        if not settings == None:
+            for path in settings.keys():
+                commands = settings.get(path)
+                # path, cmd = entry.split('::', 1)
+                if file.startswith(path) and len(commands) > 0:
+                    print("Command on Save:\n")
+                    for command in commands:
+                        p = subprocess.Popen([command], shell=True, stdout=subprocess.PIPE)
+                        out, err = p.communicate()
+                        print (out.decode('utf-8'))
